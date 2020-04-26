086c827519e14808b1ad3062c9e81fb846f40dbd
yu.i <vamos.ishida@icloud.com>
2020-04-26 13:39:23 +0900

    完成したが、タスクを入力しても保存されていない。

--- taskapp/AppDelegate.swift
+++ taskapp/AppDelegate.swift
@@ -7,17 +7,31 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+// UNUserNotificationCenterDelegateを追加
+class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // ユーザに通知の許可を求める
+        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert,.sound]){(granted,error)in
+         //Enable or disable features based on authorization
+        }
+        center.delegate = self     // 追加
         return true
     }
 
+    // アプリがフォアグラウンドの時に通知を受け取ると呼ばれるメソッド --- ここから ---
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    } // --- ここまで追加 ---
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,10 +7,43 @@
 //
 
 import UIKit
+import RealmSwift   // ←追加
 
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    // 入力画面から戻ってきた時に TableView を更新させる
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    // segue で画面遷移する時に呼ばれる
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        } else {
+            let task = Task()
+
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+
+            inputViewController.task = task
+        }
+    }
+    
+    //Realmインスタンスを取得する
+    let realm = try! Realm()  // ←追加
+    
+    //DB内のタスクが格納されるリスト
+    //日付の近い順でソート：昇順
+    //以降内容をアップデートするとリスト内は自動的に更新される
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)  // ←追加
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +54,27 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     //UITableViewDataSourceプロトコル必須メソッド
     // データの数（＝セルの数）を返すメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return 0
+        return taskArray.count //return 0 →修正
     }
     
     //UITableViewDataSourceプロトコル必須メソッド
     // 各セルの内容を返すメソッド
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        // Fetch a cell of the appropriate type.
+       //再利用可能な cell を得る
        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-       
+        
+    // Cellに値を設定する.  --- ここから ---
+    let task = taskArray[indexPath.row]
+    cell.textLabel?.text = task.title
+
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd HH:mm"
+
+    let dateString:String = formatter.string(from: task.date)
+    cell.detailTextLabel?.text = dateString
+    // --- ここまで追加 ---
+
        // Configure the cell’s contents.
        //cell.textLabel!.text = "Cell text"
            
@@ -44,11 +89,33 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         return .delete
     }
+    
     // Delete ボタンが押された時に呼ばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-    }
+    
+    // --- ここから ---
+    if editingStyle == .delete {
+        // 削除するタスクを取得する
+        let task = self.taskArray[indexPath.row]
 
+        // ローカル通知をキャンセルする
+        let center = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
 
+        // データベースから削除する
+        try! realm.write {
+            self.realm.delete(task)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
 
+        // 未通知のローカル通知一覧をログ出力
+        center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+            for request in requests {
+                print("/---------------")
+                print(request)
+                print("---------------/")
+            }
+        }
+    } // --- ここまで変更 ---
+        }
 }
-
